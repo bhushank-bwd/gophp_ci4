1d957b15c1d2523628fbd8292723ec9e94a16e63
bhushank <bhushank@noitavonne.com>
2023-04-15 10:40:26 +0530

    09 Remapping method calls in Controller class | CodeIgniter 4 Tutorials

--- ci4/app/Controllers/User.php
+++ ci4/app/Controllers/User.php
@@ -8,6 +8,28 @@ class User extends BaseController
 {
     public function index()
     {
-        //
+        echo "This is index";
+    }
+    public function test($name){
+        echo "Test ".$name;
+    }
+    public function address($state,$city){
+        echo "Address ".$city." ".$state;
+    }
+    public function _remap($method,$p1,$p2){
+
+        // one way
+        // if($method === 'test'){
+        //     return $this->$method($p1);
+        // }else if($method === 'address'){
+        //     return $this->$method($p1,$p2);
+        // }else{
+        //     return $this->index();
+        // }
+
+        if(method_exists($this,$method)){
+            return $this->$method($p1,$p2);
+        }
+        throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
     }
 }
