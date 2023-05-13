45fd5920e04a2edc7ee4c5137fd6b30f9b2c9ac0
bhushank <bhushank@noitavonne.com>
2023-05-13 10:30:19 +0530

    45) Controller Filters in CodeIgniter 4

---
+++ ci4/app/Filters/DateFilter.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Filters;
+
+use \CodeIgniter\Filters\FilterInterface;
+use \CodeIgniter\HTTP\RequestInterface;
+use \CodeIgniter\HTTP\ResponseInterface;
+
+class DateFilter implements FilterInterface
+{
+    public function before(RequestInterface $req, $arguments = null)
+    {
+        echo "Before ".date("l Y-m-d h:i:A");
+    }
+    public function after(RequestInterface $req, ResponseInterface $resp, $arguments = null)
+    {
+        echo "After ".date("l Y-m-d h:i:A");
+    }
+}
+
+?>
